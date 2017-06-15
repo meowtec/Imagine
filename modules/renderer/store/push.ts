4ff27eae6f5688af10d4d5067cd80d71acb4c095
meowtec <bertonzh@gmail.com>
2017-06-15 22:31:07 +0800

    feat: if add only one image, show it on alone mode

--- modules/renderer/store/push.ts
+++ modules/renderer/store/push.ts
@@ -6,7 +6,11 @@ import { IImageFile, IpcChannel } from '../../common/constants'
 
 const listenPush = (store: Store<IState>) => {
   ipcRenderer.on(IpcChannel.FILE_SELECTED, (event: any, data: IImageFile[]) => {
+    const emptyTask = store.getState().tasks.length === 0
     store.dispatch(actions.taskAdd(data))
+    if (emptyTask && data.length === 1) {
+      store.dispatch(actions.taskDetail(data[0].id))
+    }
   })
 }
 
